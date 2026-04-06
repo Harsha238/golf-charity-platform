9ee232f795825e4d3daf3d2b2aa999c86674accb
Harsha238 <harshithasai556@gmail.com>
2026-04-06 21:18:34 +0530

    fix winner model

--- backend/models/Winner.js
+++ backend/models/Winner.js
@@ -1,11 +1,26 @@
 import mongoose from "mongoose";
 
 const winnerSchema = new mongoose.Schema({
-  userId: String,
-  name: String,
-  score: Number,
-  prize: Number,
-  date: Date
+  userId: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    default: "Player"
+  },
+  score: {
+    type: Number,
+    default: 0
+  },
+  prize: {
+    type: Number,
+    default: 0
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 export default mongoose.model("Winner", winnerSchema);
